5be9b33eb8b800d9515529d8f76138455cd94e82
ulmanyar <ulmanyar@gmail.com>
2022-08-26 21:41:55 +0200

    Add first iteration of keymap

---
+++ keyboards/splitkb/kyria/keymaps/ulmanyar/config.h
@@ -0,0 +1,37 @@
+/* Copyright 2022 Thomas Baart <thomas@splitkb.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
+#define SPLIT_LAYER_STATE_ENABLE
+
+#ifdef TAP_DANCE_ENABLE
+#    define TAPPING_TERM 175
+#endif
+
+#ifdef RGBLIGHT_ENABLE
+#    define RGBLIGHT_SLEEP           # Turn RGB underglow off when power off
+#    define RGBLIGHT_ANIMATIONS
+#    define RGBLIGHT_HUE_STEP  8
+#    define RGBLIGHT_SAT_STEP  8
+#    define RGBLIGHT_VAL_STEP  8
+#    define RGBLIGHT_LIMIT_VAL 150
+#endif
+
+// Lets you roll mod-tap keys
+#define IGNORE_MOD_TAP_INTERRUPT
