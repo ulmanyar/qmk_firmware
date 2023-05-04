093f1fd902039ad00a184360a7383f7ba4b4f027
ulmanyar <ulmanyar@gmail.com>
2023-05-04 20:01:13 +0200

    Enable key overrides on the Kyria

--- keyboards/splitkb/kyria/keymaps/ulmanyar/rules.mk
+++ keyboards/splitkb/kyria/keymaps/ulmanyar/rules.mk
@@ -3,6 +3,8 @@ OLED_DRIVER = SSD1306   # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow, approximately 4865 bytes
 COMBO_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
 
+SRC += ulmanyar.c
 SRC += oneshot.c
 SRC += swapper.c
