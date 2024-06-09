6d7e1f2ee97d38e3f987652b71a95de3c6174e75
ulmanyar <ulmanyar@gmail.com>
2024-06-09 22:32:35 +0200

    Disable mousekey and add rgb sleep to Niu

--- keyboards/kbdfans/niu_mini/keymaps/ulmanyar/rules.mk
+++ keyboards/kbdfans/niu_mini/keymaps/ulmanyar/rules.mk
@@ -2,7 +2,7 @@
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
+MOUSEKEY_ENABLE = no       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = yes        # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
